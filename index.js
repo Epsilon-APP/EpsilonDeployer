fc44c87e959b90688eeb893a129ec5132c9e7135
EliXorZz <dylan.battig@gmail.com>
2022-09-29 17:50:55 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const core = require('@actions/core');
+
+const fs = require('fs')
+const path = require('path')
+
+const axios = require('axios')
+
+try {
+  const host = core.getInput('host')
+  const secret = core.getInput('secret')
+
+  const type = core.getInput('type')
+  const jar = core.getInput('jar')
+  const destination = core.getInput('destination')
+
+  pushFile(host, type, destination, jar, secret)
+} catch (error) {
+  core.setFailed(error.message)
+}
+
+async function pushFile(host, type, destination, file, secret) {
+  var FormData = require('form-data');
+
+  const formData = new FormData()
+  formData.append('upload', fs.readFileSync(file), path.basename(file))
+
+  await axios.post(`${host}/${type}/${destination}/plugins/push`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+      'Proxy-Authorization': secret
+    }
+  })
+}
